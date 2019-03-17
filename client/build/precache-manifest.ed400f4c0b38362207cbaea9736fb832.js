7b893ce01a56f02bfa95e50e5489588707732f38
Anton Kogan <34128946+chinhippy@users.noreply.github.com>
2019-03-16 21:41:13 -0500

    w /build

---
+++ client/build/precache-manifest.ed400f4c0b38362207cbaea9736fb832.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b121c8e14330739cafbb",
+    "url": "/static/js/main.97ee1ee1.chunk.js"
+  },
+  {
+    "revision": "afd1f750c75ee85018b7",
+    "url": "/static/js/2.9fdfa0d9.chunk.js"
+  },
+  {
+    "revision": "b121c8e14330739cafbb",
+    "url": "/static/css/main.392efbcb.chunk.css"
+  },
+  {
+    "revision": "ea7b8b71e75f5192392ac22cc660180b",
+    "url": "/index.html"
+  }
+];
